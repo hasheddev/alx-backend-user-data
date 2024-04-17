b7c2583f33e5094cb88d54c214ff6f27709c73bb
Shola <khidirshola@gmail.com>
2024-04-17 21:38:17 +0100

    add session auth class

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.getenv("AUTH_TYPE", None)
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+elif auth_type == 'auth':
+    auth = Auth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
+
+
+@app.before_request
+def before_request():
+    """ processes request before response """
+    if auth:
+        excluded_paths = [
+                '/api/v1/status/', '/api/v1/unauthorized/',
+                '/api/v1/forbidden/'
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            user = auth.current_user(request)
+            if user is None:
+                abort(403)
+            request.current_user = user
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden access handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """ Not authorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for calss SessionAuth  """
+from . import auth
+import uuid
+
+
+class SessionAuth(auth.Auth):
+    """ Class that implements session authentication for app """
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
