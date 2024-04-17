c0d53792bd37b4e241e2522136f5d456f546c3b9
Shola <khidirshola@gmail.com>
2024-04-17 21:44:47 +0100

    update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ import uuid
 class SessionAuth(auth.Auth):
     """ Class that implements session authentication for app """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id """
         if user_id is None or type(user_id) is not str:
